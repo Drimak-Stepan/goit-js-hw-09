536092e08bb5b378bfea85c0f690f38c01375e38
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-25 10:40:41 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,33 @@
-import throttle from 'lodash.throttle';
-
 const refs = {
-  start: document.querySelector('[data-start]'),
-  stop: document.querySelector('[data-stop]'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
   body: document.querySelector('body'),
 };
 
-refs.start.addEventListener('click', changeColor);
-
-function changeColor() {
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
-  const randomColor = getRandomHexColor();
-  refs.body.style.backgroundColor = randomColor;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyBackgroundColor = {
+  id: null,
+  start() {
+    refs.start.setAttribute('disabled', '');
+    this.id = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.id);
+    refs.start.removeAttribute('disabled', '');
+  },
+};
+
+refs.start.addEventListener(
+  'click',
+  bodyBackgroundColor.start.bind(bodyBackgroundColor)
+);
+refs.stop.addEventListener(
+  'click',
+  bodyBackgroundColor.stop.bind(bodyBackgroundColor)
+);
