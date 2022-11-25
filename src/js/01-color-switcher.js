c55af3da55c7517f2b7c3aab6cf1f0674bf39236
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-25 09:49:48 +0200

    rep

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.start.addEventListener('click', changeColor);
+
+function changeColor() {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+}
